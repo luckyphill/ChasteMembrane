2d4fefb423d95462aae6e944f8006ab482baca03
luckyphill <phillip.j.brown@adelaide.edu.au>
2018-12-04 11:25:11 +1030

    prepare to load on mac

--- transfer_between_phoenix.sh
+++ transfer_between_phoenix.sh
@@ -8,7 +8,7 @@
 # rsync -avz --progress -f"- .*/" -f"- output/" -f"+ */" -f"+ */*" -f"- *"   a1738927@phoenix.adelaide.edu.au:/fast/users/a1738927/Chaste/projects/ChasteMembrane/ .
 
 # transfers Matlab code for creating the parameter space sweeps
-# adds subfolders and their contents: -f"- */"
+# adds subfolders and their contents: -f"+ */"
 # adds .cpp .hpp .txt .py .m and .sh files: -f"+ *.m" -f"+ *.sh"
 # ignores any other file eg. pdfs: -f"- *.*"
 # removes anything in the destination folder that isn't being transfered: --delete
